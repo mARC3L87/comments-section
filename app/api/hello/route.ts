049d16e8a1e9e8f03b6e65be68546dc04073d84b
Marcel Lukomski <marcel.lukomski@gmail.com>
2023-04-13 20:55:01 +0200

    Add style to single post.

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,10 @@
+import { NextResponse } from 'next/server';
+import { db } from '../../../firebase/config';
+import { collection, getDocs } from 'firebase/firestore';
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  const fetchData = await getDocs(collection(db, 'comments'));
+  const res = fetchData.docs.map((doc) => doc.data());
+  console.log(res[0].createdAt);
+  return NextResponse.json(res);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,36 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Image from 'next/image';
+import replyIcon from '../public/images/icon-reply.svg';
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className='bg-neutral-white max-w-[1440px] w-1/2 flex p-5 rounded-md'>
+        <div className='flex flex-col items-center h-20 justify-center rounded-lg bg-neutral-veryLightGray py-1 px-2 text-xl text-primary-lightGrayishBlue font-bold'>
+          <p>+</p>
+          <p className='text-primary-moderateBlue text-base'>12</p>
+          <p>-</p>
+        </div>
+        <div className='flex flex-col w-full ml-5'>
+          <div className=' flex-row flex justify-between h-5'>
+            <div className='flex'>
+              <p className='text-neutral-darkBlue font-bold'>amyrobson</p>
+              <p className='text-neutral-grayishBlue font-bold ml-2'>
+                1 month ago
+              </p>
+            </div>
+            <div className='flex flex-row items-center'>
+              <Image src={replyIcon} alt='reply' className='h-[15px]' />
+              <p className='text-primary-moderateBlue font-bold mx-1'>Reply</p>
+            </div>
+          </div>
+          <div className='text-neutral-grayishBlue font-medium'>
+            <p className='py-3'>
+              Impressive! Though it seems the drag feature could be improved.
+              But overall it looks incredible.You've nailed the design and the
+              responsiveness at various breakpoints works really well.
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
