439fd8e6131862a53c296b26735ce23dba8e153a
Marcel Lukomski <marcel.lukomski@gmail.com>
2023-04-27 19:30:26 +0200

    Add responsive mobile view.

--- app/components/ReplyPost.tsx
+++ app/components/ReplyPost.tsx
@@ -2,10 +2,14 @@ import Image from 'next/image';
 import juliusomo from '../../public/images/avatars/image-juliusomo.png';
 const ReplyPost = () => {
   return (
-    <div className='bg-neutral-white max-w-[1440px] w-1/2 flex justify-between	 p-5 rounded-md mt-6'>
-      <Image src={juliusomo} alt='juliusomo' className='w-10 h-10' />
+    <div className='md:flex-nowrap bg-neutral-white max-w-[1440px] w-1/2 flex flex-wrap justify-between p-5 rounded-md mt-6'>
+      <Image
+        src={juliusomo}
+        alt='juliusomo'
+        className='md:order-first md:w-10 md:h-10 w-8 h-8'
+      />
       <textarea
-        className='px-5 placeholder:text-neutral-grayishBlue border border-neutral-lightGray resize-none py-2 text-neutral-grayishBlue rounded-md w-full h-28 mx-3'
+        className='md:mx-3 order-first px-5 placeholder:text-neutral-grayishBlue border border-neutral-lightGray resize-none py-2 text-neutral-grayishBlue rounded-md w-full h-28 mb-3'
         placeholder='Add a comment...'
       ></textarea>
       <button
--- app/components/SinglePost.tsx
+++ app/components/SinglePost.tsx
@@ -1,24 +1,24 @@
 import Image from 'next/image';
 import replyIcon from '../../public/images/icon-reply.svg';
+import amyrobson from '../../public/images/avatars/image-amyrobson.png';
 const SinglePost = () => {
   return (
-    <div className='bg-neutral-white max-w-[1440px] w-1/2 flex p-5 rounded-md mt-6'>
-      <div className='flex flex-col items-center h-20 justify-center rounded-lg bg-neutral-veryLightGray py-1 px-2 text-xl text-primary-lightGrayishBlue font-bold'>
+    <div className='md:flex-nowrap md:items-start flex-wrap bg-neutral-white max-w-[1440px] w-1/2 flex p-5 rounded-md mt-6 justify-between'>
+      <div className='md:order-first md:flex-col md:justify-center md:h-20 md:px-0 order-3 flex flex-row items-center rounded-lg bg-neutral-veryLightGray md:pt-2 px-3 text-xl text-primary-lightGrayishBlue font-bold'>
         <p className='hover:text-primary-moderateBlue cursor-pointer'>+</p>
-        <p className='text-primary-moderateBlue text-base'>12</p>
-        <p className='hover:text-primary-moderateBlue cursor-pointer'>-</p>
+        <p className='text-primary-moderateBlue text-base mx-3'>12</p>
+        <p className='hover:text-primary-moderateBlue cursor-pointer md:mb-1 pb-1'>
+          -
+        </p>
       </div>
-      <div className='flex flex-col w-full ml-5'>
-        <div className=' flex-row flex justify-between h-5'>
+      <div className='flex flex-col w-full md:ml-5'>
+        <div className=' flex-row flex justify-between h-5 mb-3'>
           <div className='flex'>
+            <Image src={amyrobson} alt='amyrobson' className='w-8 h-8 mr-3' />
             <p className='text-neutral-darkBlue font-bold'>amyrobson</p>
-            <p className='text-neutral-grayishBlue font-bold ml-2'>
+            <p className='text-neutral-grayishBlue font-medium ml-2'>
               1 month ago
             </p>
-          </div>
-          <div className='flex flex-row items-center hover:opacity-30 cursor-pointer'>
-            <Image src={replyIcon} alt='reply' className='h-[15px]' />
-            <p className='text-primary-moderateBlue font-bold mx-1'>Reply</p>
           </div>
         </div>
         <div className='text-neutral-grayishBlue font-medium'>
@@ -28,6 +28,10 @@ const SinglePost = () => {
             responsiveness at various breakpoints works really well.
           </p>
         </div>
+      </div>
+      <div className='md:order-3 order-4 flex flex-row items-center hover:opacity-30 cursor-pointer'>
+        <Image src={replyIcon} alt='reply' className='h-[15px]' />
+        <p className='text-primary-moderateBlue font-bold mx-1'>Reply</p>
       </div>
     </div>
   );
